4e2d30172b066f8eb032baeeb6d4c4fc40346637
ryoff <ryoffes@gmail.com>
2012-05-17 02:14:17 +0900

    second commite

--- jquery.selectarea.js
+++ jquery.selectarea.js
@@ -5,11 +5,7 @@
  *      ryoff
  */
 (function(jQuery) {
-    jQuery.fn.selectArea = function(config){
-        if (!!window.opera&&!window.console) {console={log:opera.postError}};
-//        var defaults = {
-//        };
-//        var opt = jQuery.extend(defaults, config);
+    jQuery.fn.selectArea = function(fn){
         // elementの状態
         var obj_state = {
             x: this.position().left,
@@ -24,17 +20,18 @@
             py: 0,
             ox: 0, // pageX - obj_state.x
             oy: 0,
-            box :   jQuery(document.createElement('div')).css({
+            box :   jQuery(document.createElement('layer')).css({
                         width: 1,
                         height: 1,
                         position: 'absolute',
                         top: 1,
                         left: 1,
                         border: '1px dotted gray',
-                        zIndex: '1',
-                        id: 'selectAreaBox'
+                        display: 'none',
                     })
         };
+        // 範囲枠作成
+        $("body").prepend(state.box);
 
         
         // bind mouse down
@@ -48,8 +45,8 @@
                      .css('left', state.sx)
                      .css('width', 1)
                      .css('height', 1)
-            // 範囲枠作成
-            $("body").prepend(state.box);
+                     .css('display', 'block')
+            return false;
         };
 
         // bind mouse move
@@ -57,13 +54,25 @@
         state.box.bind('mousemove.selectArea', mouseMoveSelectArea);
         function mouseMoveSelectArea(e){
             if (!state.down) return false;
-            state.px = e.pageX;
             state.py = e.pageY;
-            state.box.css('width', state.px - state.sx)
-                     .css('height', state.py - state.sy);
-            
+            state.px = e.pageX;
+            if (state.sy <= state.py) {
+                state.box.css('height', state.py - state.sy);
+            }
+            else {
+                state.box.css('top', state.py);
+                state.box.css('height', state.sy - state.py);
+            }
+            if (state.sx <= state.px) {
+                state.box.css('width', state.px - state.sx);
+            }
+            else {
+                state.box.css('left', state.px);
+                state.box.css('width', state.sx - state.px);
+            }
             // debug
-            $('#info').text("state: " + state.down);
+            $('#info').text("state: " + state.down + '   px:'+state.px+' py:'+state.py+' sx:'+state.sx+' sy:'+state.sy);
+            return false;   
         };
 
         // bind mouse up
@@ -73,6 +82,13 @@
             state.down = false;
             state.ox = state.px - obj_state.x;
             state.oy = state.py - obj_state.y;
+            var rtn = {
+                'ay': parseInt(state.box.css('top')) - obj_state.y,
+                'ax': parseInt(state.box.css('left')) - obj_state.x,
+                'by': parseInt(state.box.css('top')) + parseInt(state.box.css('top')) - obj_state.y,
+                'bx': parseInt(state.box.css('left')) + parseInt(state.box.css('left')) - obj_state.x,
+            };
+            fn(rtn);
         };
     }
 })(jQuery);
